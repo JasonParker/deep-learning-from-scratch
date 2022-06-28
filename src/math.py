44b20dae3b9550c5c6970041b2989c9fa4ba931c
Jason Parker <jason.parker2@hcahealthcare.com>
2022-06-28 16:19:27 -0700

    Activation functions

--- src/math.py
+++ src/math.py
@@ -1,14 +1,43 @@
-import numpy as numpy
+import numpy as np
+from numpy import ndarray
+from typing import Callable
+
+
+def binary_step_function(x: ndarray,
+                         threshold: float) -> ndarray:
+    '''
+    Apply binary step function to each element in ndarray using
+    float threshold as the basis for activation.
+    '''
+    def binary_step(item):
+        if item < threshold:
+            return 0
+        else:
+            return 1
+    return np.array([binary_step(item) for item in x])
 
 
 def derivative(func: Callable[[ndarray], ndarray],
                input_: ndarray,
                delta: float = 0.001) -> ndarray:
     '''
-    Evaluatves the derivative of a function "func" at every element in 
+    Evaluates the derivative of a function "func" at every element in 
     the "input_" array.
     '''
     return (func(input_ + delta) - func(input_ - delta)) / (2 * delta)
+
+
+def elu_function(x: ndarray, slope: float) -> ndarray:
+    '''
+    Apply exponential linear unit function to each element in ndarray using
+    float slope as the basis for activation.
+    '''
+    def elu(x, a):
+        if x<0:
+            return a*(np.exp(x)-1)
+        else:
+            return x
+    return np.array([elu(item, slope) for item in x])
 
 
 def leaky_relu(x: ndarray) -> ndarray:
@@ -19,6 +48,14 @@ def leaky_relu(x: ndarray) -> ndarray:
     https://paperswithcode.com/method/leaky-relu
     '''
     return np.maximum(0.2 * x, x)
+
+
+def linear_activation(x: ndarray, slope: float) -> ndarray:
+    '''
+    Apply linear function to each element in ndarray using
+    float slope as the basis for activation.
+    '''
+    return x * slope
 
 
 def relu(x: ndarray) -> ndarray:
@@ -38,11 +75,33 @@ def sigmoid(x: ndarray) -> ndarray:
     return 1 / (1 + np.exp(-x))
 
 
+def softmax_function(x: ndarray) -> ndarray:
+    '''
+    Apply softmax function
+    '''
+    z = np.exp(x)
+    return z/z.sum()
+
+
 def square(x: ndarray) -> ndarray:
     '''
     Square each element in the input ndarray.
     '''
     return np.power(x, 2)
+
+
+def swish_function(x: ndarray) -> ndarray:
+    '''
+    Apply swich function
+    '''
+    return x/(1-np.exp(-x))
+
+
+def tanh_function(x: ndarray) -> ndarray:
+    '''
+    Apply the tanh function to each element in the input ndarray.
+    '''
+    return (2/(1 + np.exp(-2*x))) -1
 
 
 
